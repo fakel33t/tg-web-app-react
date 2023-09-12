ca40c46a6618c072dbdaf4d4c1ac1ea4b453d0d4
fakel33t <zorgck@mail.ru>
2023-09-12 14:05:41 +0300

    productlistik

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -5,10 +5,10 @@ import {useTelegram} from "../../hooks/useTelegram";
 import {useCallback , useEffect} from "react"
 
 const products = [
-    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '1', title: 'Кроссовки', price: 5000, description: 'Синего цвета, прямые'},
     {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
     {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
+    {id: '4', title: 'Кроссовки New Balance 2002r', price: 122, description: 'Зеленого цвета, теплая'},
     {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
     {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
     {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
